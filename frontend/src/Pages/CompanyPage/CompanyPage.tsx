0f8bd389da18db6385d07b243963719f5387caeb
hau-ha <haduyhau1995@gmail.com>
2024-05-23 02:52:42 +0300

    Add GetCompanyProfile APi

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { CompanyProfile } from "../../company";
+import { getCompanyProfile } from "../../api";
 
 interface Props {}
 
 const CompanyPage = (props: Props) => {
-  return (
-    <div>CompanyPage</div>
-  )
-}
+  let { ticker } = useParams();
 
-export default CompanyPage
+  const [company, setCompany] = useState<CompanyProfile>();
+
+  useEffect(() => {
+    const getProlfileInit = async () => {
+      const result = await getCompanyProfile(ticker!);
+      setCompany(result?.data[0]);
+    };
+    getProlfileInit();
+  }, []);
+
+  return (
+    <>
+      {company ? (
+        <div>{company.companyName}</div>
+      ) : (
+        <div>Company not found!</div>
+      )}
+    </>
+  );
+};
+
+export default CompanyPage;
