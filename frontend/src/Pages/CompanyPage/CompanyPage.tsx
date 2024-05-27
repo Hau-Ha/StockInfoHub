6098e347345340b719b0290685e427e89d60174b
hau-ha <haduyhau1995@gmail.com>
2024-05-27 22:39:42 +0300

    add dasboad Info

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -4,6 +4,7 @@ import { CompanyProfile } from "../../company";
 import { getCompanyProfile } from "../../api";
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import CompanyDashboard from "../../Components/CompanyDashboard/CompanyDashboard";
+import Tile from "../../Components/Tile/Tile";
 
 interface Props {}
 
@@ -25,7 +26,15 @@ const CompanyPage = (props: Props) => {
       {company ? (
         <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
           <Sidebar />
-          <CompanyDashboard />
+          <CompanyDashboard ticker={ticker!}>
+            <Tile title="Company Name" subTitle={company.companyName} />
+            <Tile title="Price" subTitle={"$" + company.price.toString()} />
+            <Tile title="DCF" subTitle={"$" + company.dcf.toString()} />
+            <Tile title="Sector" subTitle={company.sector} />
+            <p className="bg-white shadow rounded text-medium font-medium text-gray-900 p-3 mt-1 m-4">
+              {company.description}
+            </p>
+          </CompanyDashboard>
         </div>
       ) : (
         <div>Company not found!</div>
--- frontend/src/Pages/DesignGuide/DesignGuide.tsx
+++ frontend/src/Pages/DesignGuide/DesignGuide.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import Table from "../../Components/Table/Table";
+import RatioList from "../../Components/RatioList/RatioList";
+import { TestDataCompany } from "../../Components/Table/testData";
+import { CompanyKeyMetrics } from "../../company";
 
-interface Props {}
+type Props = {};
+
+const data = TestDataCompany;
+
+const tableConfig = [
+  {
+    label: "symbol",
+    render: (company: any) => company.symbol,
+  },
+];
 
 const DesignGuide = (props: Props) => {
   return (
-    <div>DesignGuide</div>
-  )
-}
+    <>
+      <h1>
+      
+      </h1>
+      <RatioList config={tableConfig} data={data} />
+      <Table config={tableConfig} data={data} />
+      <h3>
+      
+      </h3>
+    </>
+  );
+};
 
-export default DesignGuide
+export default DesignGuide;
